c5406a18edfbaaf9720be41659384cc8a60af714
Chase Crossley <ChaseDakota.Crossley@calbaptist.edu>
2020-05-15 12:41:32 -0700

    implemented basic GET and POST requests for all Class Types

--- REST_API/urls.py
+++ REST_API/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
+    path('api/LocationInStore/', views.LocationInStoreCreate.as_view()),
+    path('api/ScopeOfWork/', views.ScopeOfWorkCreate.as_view()),
+    path('api/ScopeOfWorkStatus/', views.ScopeOfWorkStatusCreate.as_view()),
+    path('api/SchedulingWork/', views.SchedulingWorkCreate.as_view()),
+    path('api/OrderMaterial/', views.OrderMaterialCreate.as_view()),
+    path('api/MaterialList/', views.MaterialListCreate.as_view()),
+    path('api/MaterialItem/', views.MaterialItemCreate.as_view()),
     path('api/WorkOrderStatus/', views.WorkOrderStatusCreate.as_view()),
+    path('api/WorkOrder/', views.WorkOrderCreate.as_view()),
     path('api/Employee/', views.EmployeeCreate.as_view()),
+    path('api/ArticleNumber/', views.ArticleNumberCreate.as_view()),
+    path('api/LaborItem/', views.LaborItemCreate.as_view()),
+    path('api/ArticleNumberState/', views.ArticleNumberStateCreate.as_view()),
+    path('api/Store/', views.StoreCreate.as_view()),
+    path('api/RegionCode/', views.RegionCodeCreate.as_view()),
+    path('api/Customer/', views.CustomerCreate.as_view()),
+    path('api/MicroRegionCode/', views.MicroRegionCodeCreate.as_view()),
+    path('api/PrepWork/', views.PrepWorkCreate.as_view()),
 ]
--- REST_API/views.py
+++ REST_API/views.py
@@ -1,9 +1,41 @@
 from rest_framework_mongoengine import generics as drfme_generics
-
 from .serializers import *
-
-
 # Create your views here.
+
+
+class LocationInStoreCreate(drfme_generics.ListCreateAPIView):
+    queryset = LocationInStore.objects.all()
+    serializer_class = LocationInStoreSerializer
+
+
+class ScopeOfWorkCreate(drfme_generics.ListCreateAPIView):
+    queryset = ScopeOfWork.objects.all()
+    serializer_class = ScopeOfWorkSerializer
+
+
+class ScopeOfWorkStatusCreate(drfme_generics.ListCreateAPIView):
+    queryset = ScopeOfWorkStatus.objects.all()
+    serializer_class = ScopeOfWorkStatusSerializer
+
+
+class SchedulingWorkCreate(drfme_generics.ListCreateAPIView):
+    queryset = SchedulingWork.objects.all()
+    serializer_class = SchedulingWorkSerializer
+
+
+class OrderMaterialCreate(drfme_generics.ListCreateAPIView):
+    queryset = OrderMaterial.objects.all()
+    serializer_class = OrderMaterialSerializer
+
+
+class MaterialListCreate(drfme_generics.ListCreateAPIView):
+    queryset = MaterialList.objects.all()
+    serializer_class = MaterialListSerializer
+
+
+class MaterialItemCreate(drfme_generics.ListCreateAPIView):
+    queryset = MaterialItem.objects.all()
+    serializer_class = MaterialItemSerializer
 
 
 class WorkOrderStatusCreate(drfme_generics.ListCreateAPIView):
@@ -11,9 +43,39 @@ class WorkOrderStatusCreate(drfme_generics.ListCreateAPIView):
     serializer_class = WorkOrderStatusSerializer
 
 
+class WorkOrderCreate(drfme_generics.ListCreateAPIView):
+    queryset = WorkOrder.objects.all()
+    serializer_class = WorkOrderSerializer
+
+
+class EmployeeCreate(drfme_generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+
+class ArticleNumberCreate(drfme_generics.ListCreateAPIView):
+    queryset = ArticleNumber.objects.all()
+    serializer_class = ArticleNumberSerializer
+
+
+class LaborItemCreate(drfme_generics.ListCreateAPIView):
+    queryset = LaborItem.objects.all()
+    serializer_class = LaborItemSerializer
+
+
+class ArticleNumberStateCreate(drfme_generics.ListCreateAPIView):
+    queryset = ArticleNumberState.objects.all()
+    serializer_class = ArticleNumberStateSerializer
+
+
 class StoreCreate(drfme_generics.ListCreateAPIView):
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
+
+
+class RegionCodeCreate(drfme_generics.ListCreateAPIView):
+    queryset = RegionCode.objects.all()
+    serializer_class = RegionCodeSerializer
 
 
 class CustomerCreate(drfme_generics.ListCreateAPIView):
@@ -21,6 +83,13 @@ class CustomerCreate(drfme_generics.ListCreateAPIView):
     serializer_class = CustomerSerializer
 
 
-class EmployeeCreate(drfme_generics.ListCreateAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+class MicroRegionCodeCreate(drfme_generics.ListCreateAPIView):
+    queryset = MicroRegionCode.objects.all()
+    serializer_class = MicroRegionCodeSerializer
+
+
+class PrepWorkCreate(drfme_generics.ListCreateAPIView):
+    queryset = PrepWork.objects.all()
+    serializer_class = PrepWorkSerializer
+
+
