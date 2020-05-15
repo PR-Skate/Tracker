6b50ed793fc280f56ade8a38d9b5e6f9fae07d87
Chase Crossley <ChaseDakota.Crossley@calbaptist.edu>
2020-05-15 12:06:56 -0700

    got ride of securemongo, we secure enough

--- Class_Types/employee.py
+++ Class_Types/employee.py
@@ -1,28 +1,40 @@
 # Created By: Alex Peterson
 # Created On: 05/11/2020
 #
+import bcrypt as bcrypt
 from mongoengine import StringField, DateTimeField, EmailField, DecimalField, BooleanField, \
-    EmbeddedDocumentField, DateField
-from securemongoengine import EncryptedStringField
+    EmbeddedDocumentField, DynamicField
 
 from Class_Types.base_record import BaseRecord
 from Class_Types.Embeded_Documents.embeded_classes import Address
 import re
+import hashlib
+import os
 
-_key = b'k0b@T:2I"M/:)_@eZSPVJcBin?Y\w"(1'
 
 
 class Employee(BaseRecord):
     userName = StringField(max_length=50, required=True, unique=True)
     firstName = StringField(max_length=50, required=True)
     lastName = StringField(max_length=50, required=True)
-    birthDate = DateField(required=True)
+    birthDate = DateTimeField(required=True)
     address = EmbeddedDocumentField(Address, required=True)
     email = EmailField(required=True, unique=True)
     phone = StringField(max_length=20, required=True)
-    pin = EncryptedStringField(key=_key, min_length=4, regex=re.compile('^\d{4}'), max_length=4, required=True)
+    pin = StringField(regex=re.compile('^\d{4}'), max_length=4, required=True)
     rateOfPay = DecimalField()
     active = BooleanField(default=False)
-    passwordHash = EncryptedStringField(key=_key, min_length=8, max_length=40, required=True)
+    _passwordHashBytes = DynamicField(required=True, db_field='passwordHash')
     type = StringField(max_length=10, required=True)
     meta = {'collection': 'Employee'}
+
+    @property
+    def passwordHash(self):
+        return self._passwordHashBytes
+
+    @passwordHash.setter
+    def passwordHash(self, passwordString):
+        self._passwordHashBytes = bcrypt.hashpw(passwordString.encode('utf-8'), bcrypt.gensalt( 12 ))
+
+    def validatePassword(self, passwordVerify):
+        return bcrypt.checkpw(passwordVerify.encode('utf-8'), self._passwordHashBytes)
