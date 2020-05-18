6f8b3d676ced755dd917d1fbf714598a1d21a8b0
ReuellaJacob <reuella.jacob@calbaptist.edu>
2020-05-18 18:15:21 -0400

    last push from pi

--- pr_skate_calendar/src/demo-data/month-appointments.js
+++ pr_skate_calendar/src/demo-data/month-appointments.js
@@ -1,43 +1,59 @@
-export const appointments = [
-    //monts: 0 - Jan, 1 - Feb, 2 - March...
+export const appointments = [ //is basically a list
+    //months: 0 - Jan, 1 - Feb, 2 - March...
     {
         title: "Website Re-Design Plan",
         startDate: new Date(2020, 4, 23, 9, 30),
-        endDate: new Date(2020, 4, 23, 11, 30)
+        endDate: new Date(2020, 4, 23, 11, 30),
+        WO: "1",
+        category: "incomplete"
     },
     {
         title: "Book Flights to San Fran for Sales Trip",
         startDate: new Date(2020, 4, 23, 12, 0),
-        endDate: new Date(2020, 4, 23, 13, 0)
+        endDate: new Date(2020, 4, 23, 13, 0),
+        WO: "2",
+        category: "complete"
     },
     {
         title: "Final Budget Review",
         startDate: new Date(2020, 4, 24, 12, 0),
-        endDate: new Date(2020, 4, 24, 13, 35)
+        endDate: new Date(2020, 4, 24, 13, 35),
+        WO: "3",
+        category: "incomplete"
     },
     {
         title: "Upgrade Personal Computers",
         startDate: new Date(2020, 4, 25, 15, 15),
-        endDate: new Date(2020, 4, 25, 16, 30)
+        endDate: new Date(2020, 4, 25, 16, 30),
+        WO: "4",
+        category: "incomplete"
     },
     {
         title: "Customer Workshop",
         startDate: new Date(2020, 4, 26, 11, 0),
-        endDate: new Date(2020, 4, 26, 12, 0)
+        endDate: new Date(2020, 4, 26, 12, 0),
+        WO: "5",
+        category: "complete"
     },
     {
         title: "Prepare 2015 Marketing Plan",
         startDate: new Date(2020, 4, 26, 11, 0),
-        endDate: new Date(2020, 4, 26, 13, 30)
+        endDate: new Date(2020, 4, 26, 13, 30),
+        WO: "6",
+        category: "incomplete"
     },
     {
         title: "Brochure Design Review",
         startDate: new Date(2020, 4, 26, 14, 0),
-        endDate: new Date(2020, 4, 26, 15, 30)
+        endDate: new Date(2020, 4, 26, 15, 30),
+        WO: "7",
+        category: "incomplete"
     },
     {
         title: "Vacation",
         startDate: new Date(2020, 4, 28),
-        endDate: new Date(2020, 5, 7)
+        endDate: new Date(2020, 5, 7),
+        WO: "8",
+        category: "complete"
     }
 ];
