781d281cf59f58dc240af3d337b29855ca6ebb10
Reuella Jacob <reuella.jacob@calbaptist.edu>
2020-05-21 13:06:00 -0700

    DnD implemented

--- pr_skate_calendar/src/App.js
+++ pr_skate_calendar/src/App.js
@@ -1,69 +1,98 @@
-import * as React from "react";
-import Paper from "@material-ui/core/Paper";
-import { ViewState, EditingState } from "@devexpress/dx-react-scheduler";
+import * as React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
 import {
-    Scheduler,
-    WeekView,
-    Appointments,
-    Toolbar,
-    DateNavigator,
-    ViewSwitcher,
-    MonthView,
-    DayView,
-    TodayButton, //FIXME Make today button go to day view instead of month view
-} from "@devexpress/dx-react-scheduler-material-ui";
-
-import { DragDropProvider } from '@devexpress/dx-react-scheduler-material-ui';
-
+  Scheduler,
+  MonthView,
+  DayView,
+  WeekView,
+  ViewSwitcher,
+  Appointments,
+  Toolbar,
+  DateNavigator,
+  AppointmentTooltip,
+  AppointmentForm,
+  EditRecurrenceMenu,
+  TodayButton,
+  DragDropProvider,
+} from '@devexpress/dx-react-scheduler-material-ui';
 import { appointments } from "./demo-data/month-appointments";
 
-const draggingGroupName = 'appointmentsGroup';
-
 export default class App extends React.PureComponent {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            data: appointments,
-            currentViewName: "month"
-        };
-        this.currentViewNameChange = currentViewName => {
-            this.setState({ currentViewName });
-        };
-    }
+    this.state = {
+      data: appointments,
+      currentDate: new Date('2020-05-21'),
+      currentViewName: "Month",
+    };
 
-    render() {
-        const { data, currentViewName } = this.state;
+    this.onCommitChanges = this.commitChanges.bind(this);
 
-        return (
-            <Paper>
-            <Scheduler data={data}>
-            <EditingState
-                editingAppointment={({data})}
-            />
+    this.currentViewNameChange = currentViewName => {
+        this.setState({ currentViewName });
+    };
+  }
+
+  commitChanges({ added, changed, deleted }) {
+    this.setState((state) => {
+      let { data } = state;
+      if (added) {
+        const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+        data = [...data, { id: startingAddedId, ...added }];
+      }
+      if (changed) {
+        data = data.map(appointment => (
+          changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
+      }
+      if (deleted !== undefined) {
+        data = data.filter(appointment => appointment.id !== deleted);
+      }
+      return { data };
+    });
+  }
+
+  render() {
+    const { data, currentViewName, currentDate } = this.state;
+
+    return (
+      <Paper>
+        <Scheduler
+          data={data}
+        >
             <ViewState
-                defaultCurrentDate="2020-05-27"
                 currentViewName={currentViewName}
                 onCurrentViewNameChange={this.currentViewNameChange}
+                defaultCurrentDate={currentDate}
             />
 
-        <WeekView startDayHour={10} endDayHour={19} />
+            <EditingState onCommitChanges={this.onCommitChanges} />
 
-        <MonthView />
+            <MonthView />
+            <WeekView startDayHour={10} endDayHour={19} />
+            <DayView />
 
-        <DayView />
-
-        <Toolbar />
+            <Toolbar />
             <TodayButton />
             <DateNavigator />
-        <ViewSwitcher />
-        <Appointments />
-        <DragDropProvider
+            <ViewSwitcher />
+
+            <EditRecurrenceMenu />
+            <Appointments />
+
+            <AppointmentTooltip
+                showCloseButton
+                showDeleteButton
+                showOpenButton
+            />
+
+            <AppointmentForm />
+            <DragDropProvider 
             allowDrag={({ allDay }) => !allDay}
-            allowResize={() => true}
-        />
+            allowResize={() => true}/>
         </Scheduler>
-        </Paper>
+      </Paper>
     );
-    }
+  }
 }
--- pr_skate_calendar/src/demo-data/month-appointments.js
+++ pr_skate_calendar/src/demo-data/month-appointments.js
@@ -4,56 +4,56 @@ export const appointments = [ //is basically a list
         title: "Website Re-Design Plan",
         startDate: new Date(2020, 4, 23, 9, 30),
         endDate: new Date(2020, 4, 23, 11, 30),
-        WO: "1",
+        id: "1",
         category: "incomplete"
     },
     {
         title: "Book Flights to San Fran for Sales Trip",
         startDate: new Date(2020, 4, 23, 12, 0),
         endDate: new Date(2020, 4, 23, 13, 0),
-        WO: "2",
+        id: "2",
         category: "complete"
     },
     {
         title: "Final Budget Review",
         startDate: new Date(2020, 4, 24, 12, 0),
         endDate: new Date(2020, 4, 24, 13, 35),
-        WO: "3",
+        id: "3",
         category: "incomplete"
     },
     {
         title: "Upgrade Personal Computers",
         startDate: new Date(2020, 4, 25, 15, 15),
         endDate: new Date(2020, 4, 25, 16, 30),
-        WO: "4",
+        id: "4",
         category: "incomplete"
     },
     {
         title: "Customer Workshop",
         startDate: new Date(2020, 4, 26, 11, 0),
         endDate: new Date(2020, 4, 26, 12, 0),
-        WO: "5",
+        id: "5",
         category: "complete"
     },
     {
         title: "Prepare 2015 Marketing Plan",
         startDate: new Date(2020, 4, 26, 11, 0),
         endDate: new Date(2020, 4, 26, 13, 30),
-        WO: "6",
+        id: "6",
         category: "incomplete"
     },
     {
         title: "Brochure Design Review",
         startDate: new Date(2020, 4, 26, 14, 0),
         endDate: new Date(2020, 4, 26, 15, 30),
-        WO: "7",
+        id: "7",
         category: "incomplete"
     },
     {
         title: "Vacation",
         startDate: new Date(2020, 4, 28),
         endDate: new Date(2020, 5, 7),
-        WO: "8",
+        id: "8",
         category: "complete"
     }
 ];
