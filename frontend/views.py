3a9bbb1173b608e57935bae54f015650b09cc16d
Chase Crossley <ChaseDakota.Crossley@calbaptist.edu>
2020-05-28 17:36:58 -0700

    Merge branch 'master' of https://github.com/PR-Skate/Tracker
    # Conflicts:
    #	frontend/urls.py

--- frontend/views.py
+++ frontend/views.py
@@ -1,18 +1,13 @@
-import re
-
 import mongoengine
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
-from Class_Types import Employee, Address, Name, RegionCode, MicroRegionCode
-from .forms import EmployeeForm, StoreForm, BaseForm
-from mongoengine.errors import *
-from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from mongoengine.errors import *
 
-from Class_Types import Customer
-from .forms import CustomerForm, MyDateForm
+from .forms import *
 
 
 # Create your views here.
@@ -139,11 +134,142 @@ def storeForm(request):
             print(storeManagerName.lastName)
         return render(request, 'frontend/storeForm.html',
                       {'form': form, 'region_code': region_code, 'micro_region_code': micro_region_code, 'cust': cust})
-    return render(request, 'frontend/storeForm.html',{'region_code': region_code, 'micro_region_code': micro_region_code, 'cust': cust})
+    return render(request, 'frontend/storeForm.html',
+                  {'region_code': region_code, 'micro_region_code': micro_region_code, 'cust': cust})
+
 
 @login_required
 def workOrderForm(request):
     return render(request, 'frontend/workOrderForm.html')
+
+
+""""REPORTS"""
+
+
+@login_required
+def customerReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=Customer, request=request)
+
+
+@login_required
+def storeReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=Store, request=request)
+
+
+@login_required
+def sowReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=ScopeOfWork, request=request)
+
+
+@login_required
+def employeeReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=Employee, request=request)
+
+
+@login_required
+def workOrderReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=WorkOrder, request=request)
+
+
+@login_required
+def articleNumberReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=ArticleNumber, request=request)
+
+
+@login_required
+def articleNumberStateReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=ArticleNumberState, request=request)
+
+
+@login_required
+def laborItemReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=LaborItem, request=request)
+
+
+@login_required
+def materialItemReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=MaterialItem, request=request)
+
+
+@login_required
+def materialListReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=MaterialList, request=request)
+
+
+@login_required
+def orderMaterialReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=OrderMaterial, request=request)
+
+
+@login_required
+def locationInStoreReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=LocationInStore, request=request)
+
+
+@login_required
+def sowStatusReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=ScopeOfWorkStatus, request=request)
+
+
+@login_required
+def microRegionCodeReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=MicroRegionCode, request=request)
+
+
+@login_required
+def regionCodeReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=RegionCode, request=request)
+
+
+@login_required
+def prepWorkReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=PrepWork, request=request)
+
+
+@login_required
+def schedulingWorkReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=SchedulingWork, request=request)
+
+
+@login_required
+def workOrderStatusReport(request):
+    if request.method == "GET":
+        return generate_table_render(model=WorkOrderStatus, request=request)
+
+
+def generate_table_render(model, request):
+    print(model.__name__)
+    records = model.objects.filter(**request.GET.dict())
+    fields = model.get_fields()
+    instances = list()
+    for record in records:
+        print('id: {}'.format(record.id), end='\n')
+        attributes = {}
+        for field in fields:
+            field_name = record._reverse_db_field_map.get(field)
+            attributes.update({field: record.__getattribute__(field_name)})
+
+        instances.append(attributes)
+
+    return render(request, 'frontend/table_temp.html',
+                  {'table_name': model.__name__, 'fields': model.get_fields(), 'instances': instances})
 
 
 def try_to_save(model, form, request):
