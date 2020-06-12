b6525b2e2f43ed435883f2aff1572cc7cb721cdc
Chase Crossley <ChaseDakota.Crossley@calbaptist.edu>
2020-06-11 17:14:59 -0700

    added support for id field update through form
    Signed-off-by: Chase Crossley <ChaseDakota.Crossley@calbaptist.edu>

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,22 +6,51 @@ urlpatterns = [
     path('', views.index),
     path('customerForm', views.customer_form),
     url(r'^customerForm/(?P<id>\w+)$', views.customer_form, name='id'),
+
     path('employeeForm', views.employee_form),
+    url(r'^employeeForm/(?P<id>\w+)$', views.employee_form, name='id'),
+
     path('prepWorkForm', views.prep_work_form),
+    url(r'^prepWorkForm/(?P<id>\w+)$', views.prep_work_form, name='id'),
+
     path('workOrderForm', views.work_order_form),
+    url(r'^workOrderForm/(?P<id>\w+)$', views.work_order_form, name='id'),
+
     path('storeForm', views.store_form),
+    url(r'^storeForm/(?P<id>\w+)$', views.store_form, name='id'),
+
     path('microRegionCodeForm', views.micro_region_form),
+    url(r'^microRegionCodeForm/(?P<id>\w+)$', views.micro_region_form, name='id'),
+
     path('regionCodeForm', views.region_form),
+    url(r'^regionCodeForm/(?P<id>\w+)$', views.region_form, name='id'),
+
     path('workOrderStatusForm', views.work_order_status_form),
+    url(r'^workOrderStatusForm/(?P<id>\w+)$', views.work_order_status_form, name='id'),
+
     path('sowForm', views.scope_of_work_form),
+    url(r'^sowForm/(?P<id>\w+)$', views.scope_of_work_form, name='id'),
+
     path('sowStatusForm', views.scope_of_work_status_form),
+    url(r'^sowStatusForm/(?P<id>\w+)$', views.scope_of_work_status_form, name='id'),
+
     path('laborItemForm', views.labor_item_form),
+    url(r'^laborItemForm/(?P<id>\w+)$', views.labor_item_form, name='id'),
+
     path('articleNumberStateForm', views.article_number_state_form),
+    url(r'^articleNumberStateForm/(?P<id>\w+)$', views.article_number_state_form, name='id'),
+
     path('articleNumberForm', views.article_number_form),
+    url(r'^articleNumberForm/(?P<id>\w+)$', views.article_number_form, name='id'),
+
     path('materialItemForm', views.material_item_form),
-    # path('materialListForm', views.material_list_form),
+    url(r'^materialItemForm/(?P<id>\w+)$', views.material_item_form, name='id'),
+
     path('locationInStoreForm', views.location_in_store_form),
+    url(r'^locationInStoreForm/(?P<id>\w+)$', views.location_in_store_form, name='id'),
+
     path('orderMaterialForm', views.order_material_form),
+    url(r'^orderMaterialForm/(?P<id>\w+)$', views.order_material_form, name='id'),
 
     path('sign-in', views.signin),
     path('logout', views.signout),
