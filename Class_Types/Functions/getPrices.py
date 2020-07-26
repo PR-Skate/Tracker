77fe8e35e856e8b87ff57330f27fb6397f4aaabf
Cameron Warren <cameronjoseph.warren@calbaptist.edu>
2020-07-25 23:28:08 -0700

    getPrices function

--- Class_Types/Functions/getPrices.py
+++ Class_Types/Functions/getPrices.py
@@ -6,13 +6,11 @@ from PR_Skate import settings
 
 def getPrices(state):
     prices = dict()
-    if state.upper() == "WA":
-        for item in AtclNum.objects:
-            print(item)
-            if item.articleNumber in prices:
-                prices[item.articleNumber] = AtclNumSt.objects.find({"state": "WA"})
-    print(AtclNum.objects.find_one({"usedByInspectionCompanyButNotPrSkate": False}))
-
+    for item in AtclNum.objects.filter(usedByInspectionCompanyButNotPrSkate=False):
+        print(item)
+        if item.articleNumber not in prices:
+            prices[item.articleNumber] = AtclNumSt.objects.get(state=state).price
+    print(prices)
 
 
 if __name__ == '__main__':
