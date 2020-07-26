b4b6262c321cce07b39ac6326c76ee28420b37bf
Chase Crossley <ChaseDakota.Crossley@calbaptist.edu>
2020-07-25 22:51:37 -0700

    Changed some import statements in getPrices.py and added a testing connection.
    Signed-off-by: Chase Crossley <ChaseDakota.Crossley@calbaptist.edu>

--- Class_Types/Functions/getPrices.py
+++ Class_Types/Functions/getPrices.py
@@ -1,16 +1,17 @@
-from pymongo import MongoClient
-from Class_Types.Labor_Items import article_number
-from Class_Types.Labor_Items import article_number_state
+from Class_Types.Labor_Items.article_number import ArticleNumber as AtclNum
+from Class_Types.Labor_Items.article_number_state import ArticleNumberState as AtclNumSt
+# TODO ONLY NEEDED FOR TESTING PRUPOSES CAN BE DELETED ONCE TESTING IS DONE
+from PR_Skate import settings
 
 
 def getPrices(state):
     prices = dict()
     if state.upper() == "WA":
-        for item in article_number.ArticleNumber.objects:
+        for item in AtclNum.objects:
             print(item)
             if item.articleNumber in prices:
-                prices[item.articleNumber] = article_number_state.ArticleNumberState.objects.find({"state": "WA"})
-    print(AN.find_one({"usedByInspectionCompanyButNotPrSkate": False}))
+                prices[item.articleNumber] = AtclNumSt.objects.find({"state": "WA"})
+    print(AtclNum.objects.find_one({"usedByInspectionCompanyButNotPrSkate": False}))
 
 
 
