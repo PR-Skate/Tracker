68c42639f1c4c1dd01601b9bd57cef8df609cb8b
Cameron Warren <cameronjoseph.warren@calbaptist.edu>
2020-07-27 21:24:56 -0700

    updateRackingLabor.py progress

--- Class_Types/Functions/getPrices.py
+++ Class_Types/Functions/getPrices.py
@@ -1,17 +1,12 @@
 from Class_Types.Labor_Items.article_number import ArticleNumber as AtclNum
 from Class_Types.Labor_Items.article_number_state import ArticleNumberState as AtclNumSt
-# TODO ONLY NEEDED FOR TESTING PRUPOSES CAN BE DELETED ONCE TESTING IS DONE
+# TODO ONLY NEEDED FOR TESTING PURPOSES CAN BE DELETED ONCE TESTING IS DONE
 from PR_Skate import settings
 
 
 def getPrices(state):
     prices = dict()
     for item in AtclNum.objects.filter(usedByInspectionCompanyButNotPrSkate=False):
-        print(item)
         if item.articleNumber not in prices:
             prices[item.articleNumber] = AtclNumSt.objects.get(state=state).price
-    print(prices)
-
-
-if __name__ == '__main__':
-    getPrices("WA")
+    return prices
--- Class_Types/Functions/updateRackingLabor.py
+++ Class_Types/Functions/updateRackingLabor.py
@@ -1,10 +1,27 @@
-from pymongo import MongoClient
+from Class_Types.Work_Order.work_order import WorkOrder as WrkOrdr
+from Class_Types.Labor_Items.article_number import ArticleNumber as AtclNum
+from Class_Types.ScopeOfWork.scope_of_work import ScopeOfWork as ScpWrkStts
+from Class_Types.Functions.getPrices import getPrices
 
 def updateRackingLabor(wo):
-    mongo_client = MongoClient()
-    WO_Records = mongo_client.get_database("Tracker").get_collection("WorkOrder")
     execptions = ["900039800", "900039801", "900039795", "Overhead Signage", "Equipment Rentals",
                   "End Cap - Wayfinding", "Emergency Inspection", "900039797", "900039798", "Refusal of Service",
                   "900039813", "900039796", "Install Paint Cages", "900039792"]
-    workOrder = WO_Records.find(wo)
-    workOrder
+    workOrder = WrkOrdr.objects.get(workOrderName=wo)
+    prices = getPrices(workOrder.fkStoreNumber.address.state)
+    items = dict
+    proceed = False
+    for item in AtclNum.objects.filter(usedByInspectionCompanyButNotPrSkate=False):
+        items[item.articleNumber] = 0.0
+    biggestGB = 0
+    for sow in ScpWrkStts.objects.filter(fkWorkOrderName=workOrder.workOrderName and ScpWrkStts.fkStatusID.status):
+        proceed = True
+        if biggestGB < sow.GB_Counter:
+            biggestGB = sow.GB_Counter
+        biggestItem = ""
+
+
+
+
+if __name__ == '__main__':
+    updateRackingLabor("myWO")
